fb5a67b065322cf5003c61be08ec2c77d5901c23
F4NT0 <gabriel.stundner@acad.pucrs.br>
2021-09-15 22:06:15 -0300

    fotos noivos background e pequenas e tempo arrumado

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -115,10 +115,10 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2015,
-                month: 6,
-                day: 28,
-                hours: 0,
+                year: 2022,
+                month: 4,
+                day: 10,
+                hours: 6,
                 minutes: 0,
                 seconds: 0,
                 words: {

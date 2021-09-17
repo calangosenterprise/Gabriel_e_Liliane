83e5de8b1b757dac5aff717b2025c9a4cfd927b3
F4NT0 <gabriel.stundner@acad.pucrs.br>
2021-09-17 15:50:41 -0300

    testando google maps

--- js/google_map.js
+++ js/google_map.js
@@ -5,7 +5,7 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+    var myLatlng = new google.maps.LatLng(-30.0077513, -51.17431125717822);
     // 39.399872
     // -8.224454
     
@@ -30,7 +30,7 @@ function init() {
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
     
-    var addresses = ['Brooklyn'];
+    var addresses = ['Av. Assis Brasil, 1144 - Santa Maria Goretti, Porto Alegre - RS, 90430-090'];
 
     for (var x = 0; x < addresses.length; x++) {
         $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {

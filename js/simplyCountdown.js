d4898e31b065afd30593ce778934e4179bb725dc
F4NT0 <gabriel.stundner@acad.pucrs.br>
2021-09-17 13:12:34 -0300

    Arrumando o contador

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -122,10 +122,10 @@
                 minutes: 0,
                 seconds: 0,
                 words: {
-                    days: 'day',
-                    hours: 'hour',
-                    minutes: 'minute',
-                    seconds: 'second',
+                    days: 'dia',
+                    hours: 'hora',
+                    minutes: 'minuto',
+                    seconds: 'segundo',
                     pluralLetter: 's'
                 },
                 plural: true,
